4b58147b1a9c5ce9c3a363406a24440bb28dffa9
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-11-18 09:50:26 -0300

    edksca

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
-function App() {
-  const [message, setMESSAGE] =  useState('Olá,mundo') 
+function App(){
+  const [message, setMESSAGE] =  useState('Olá,mundo');
+  return(
+    <div>
+      <h1>{message}</h1>
+      <button onClick={
+        ()=>{setMESSAGE('olá fui clicado');}} 
+        ></button>
+    </div>
+  )
 }
+
+export default App
