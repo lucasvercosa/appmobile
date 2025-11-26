bada360b81cd65382790ed3ed98c5a55cbaa0224
lucasvercosa <lucas.vercosa.reis@escola.pr.gov.br>
2025-11-26 09:55:31 -0300

    kdksd\k

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,46 @@
-import { useState } from "react";
+ function App(){
+  const[task,setTask] = useState([
+    {
+      id: 1,
+      title:"Estudar programação",
+      description: "Estudando programação para se tornar um desenvolvedor full stack",
+      isCompleted:false
+    }
 
-function App(){
+    {
+      id:2,
+      title:"Estudar inglês",
+      description:"Estudar inglês para se tornar fluente",
+      iscompleted: false
+    }
+
+    {
+      id:3,
+      title: "Estudar Matemática",
+      description:"Estudar matemática para desenvolver minha lógica",
+      iscompleted: false
+    }
+
+  ]);
+
+  return(
+    <h1>Gerenciando tarefas</h1>
+  );
+
+ }
+
+ export default App;
+
+
+
+
+
+
+
+
+
+
+/*function App(){
   const [message, setMESSAGE] =  useState('Olá,mundo');
   return(
     <div>
